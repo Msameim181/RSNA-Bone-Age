6547e268eef8fe3703a1a7bcf52112c79bcb7786
Mohammad Mahdi Samei <9259samei@gmail.com>
2022-05-18 16:59:15 +0430

    Add MobileNetV2_Pre31 = {Remove dropout for review result}

--- models/MobileNet/mobilenet.py
+++ models/MobileNet/mobilenet.py
@@ -121,6 +121,51 @@ class MobileNetV2_Pre2(torch.nn.Module):
         return x
 
 
+class MobileNetV2_Pre3(torch.nn.Module):
+    def __init__(self, image_channels, num_classes = 100, name: str='MobileNetV2_Pre31') -> None:
+        super(MobileNetV2_Pre2, self).__init__()
+
+        self.name = name
+        self.in_channels = image_channels
+        self.num_classes = num_classes
+
+        self.mobilenet_v2 = models.mobilenet_v2(pretrained=True)
+
+        self.mobilenet_v2.features[0][0] = torch.nn.Conv2d(image_channels, 32, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False)
+
+        self.mobilenet_v2.classifier = torch.nn.Sequential(
+            torch.nn.ReLU(),
+            # torch.nn.Dropout(0.2),
+            torch.nn.Linear(1281, 512),
+            torch.nn.ReLU(),
+            # torch.nn.Dropout(0.5),
+            torch.nn.Linear(512, num_classes),
+
+            # torch.nn.Linear(256, 128),
+            # torch.nn.Linear(128, 64),
+            # torch.nn.Linear(64, 1),
+        )
+
+
+    def forward(self, x) -> torch.Tensor:
+        y = x[1]
+        x = x[0]
+        
+        x = self.mobilenet_v2.features(x)
+
+        x = torch.nn.functional.adaptive_avg_pool2d(x, (1, 1))
+        x = torch.flatten(x, 1)
+
+        z = x
+        y = torch.unsqueeze(y, 1).to(device='cuda', dtype=torch.float32)
+        x = torch.cat((z, y), dim=1)
+
+        x = self.mobilenet_v2.classifier(x)
+
+
+        return x
+
+
 
 def MobileNet_V2(*, pretrained:bool = False, **kwargs) -> MobileNetV2:
     """
