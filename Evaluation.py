7a769cf9b1982f1a11481b0914ae840ea22c675e
Mohammad Mahdi Samei <9259samei@gmail.com>
2022-05-30 21:13:01 +0430

    Update Evaluation part, With new data loader and structure. Test it, Looks good.

--- Evaluation.py
+++ Evaluation.py
@@ -4,12 +4,18 @@ import logging
 # Deep learning libs
 import torch
 from tqdm import tqdm
+from utils.optimize_loss import *
+from utils.rich_logger import *
+from utils.dataloader import *
 
-from utils.rich_logger import make_console
 
-console = make_console()
-
-def evaluate(net, test_loader, device, criterion):
+def evaluate(
+        net, 
+        args, 
+        test_loader: DataLoader, 
+        device, 
+        criterion, 
+        log_results: bool = True):
     """
     Evaluation the model on the Evaluation set
     """
@@ -18,38 +24,70 @@ def evaluate(net, test_loader, device, criterion):
     test_loss_first = 0
     test_loss_second = 0
     correct = 0
+    
+    for _, images, gender, target, boneage, ba_minmax, ba_zscore, boneage_onehot, _ in tqdm(test_loader, total = n_eval, desc='Evaluation Round...', unit = 'img', leave=False):
 
-    for _, images, boneage, boneage_onehot, gender in tqdm(test_loader, total = n_eval, desc='Evaluation Round...', unit = 'img', leave=False):
-
-        images = torch.unsqueeze(images, 1)
-        gender = torch.unsqueeze(gender, 1)
         images = images.to(device = device, dtype = torch.float32)
 
+        gender = torch.unsqueeze(gender, 1)
         gender = gender.to(device = device, dtype = torch.float32)
 
-        target_age = boneage_onehot.to(device = device, dtype = torch.float32)
-        t_age = boneage.to(device = device, dtype = torch.float32)
+        target_age = target.to(device = device, dtype = torch.float32)
+        boneage = boneage.to(device = device, dtype = torch.float32)
+        ba_minmax = ba_minmax.to(device = device, dtype = torch.float32)
 
         with torch.no_grad():
-            output_age = net([images, gender])
-            test_loss_first += criterion(output_age, target_age)  # sum up batch loss
+            age_pred = net([images, gender])
+
+            test_loss_first += criterion(age_pred, target_age)  # sum up batch loss
             
-            # test_loss += torch.nn.functional.cross_entropy(output_age, target_age, reduction='sum').item()  # sum up batch loss
-            pred = output_age.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
-            correct += pred.eq(t_age.view_as(pred)).sum().item()
+            pred = test_loader.dataset.predict_compiler(age_pred)
+            correct += pred.eq(boneage.view_as(pred)).sum().item()
             
-            test_loss_second += torch.nn.functional.mse_loss(pred, t_age.view_as(pred))
+            # test_loss_second += torch.nn.functional.mse_loss(pred, boneage.view_as(pred))
+            test_loss_second += torch.nn.functional.mse_loss(test_loader.dataset.out_min_max_normal(pred.view_as(boneage)), ba_minmax)
     
-    acc = correct
+    accuracy = correct
     if n_eval != 0:
         test_loss_first /= n_eval
         test_loss_second /= n_eval
-        acc /= n_eval
+        accuracy /= n_eval
 
     # Logging
-    console.print(f'\n[INFO]: Evaluation set:\n'
-                 f'\tAverage loss (criterion): {test_loss_first:.4f}'
-                 f'\tAverage loss (MSE): {test_loss_second:.4f}'
-                 f'\tAccuracy: {acc * 100:.2f}% \t Correct = {correct}/{n_eval}\n')
+    if log_results:
+        print("\n")
+        rich_print(f'\n[INFO]: Evaluation set:\n'
+                f'\tAverage loss (criterion): {test_loss_first:.4f} \t Average loss (MSE): {test_loss_second:.4f}\n'
+                f'\tAccuracy: {accuracy * 100:.2f}% \t Correct = {correct}/{n_eval}\n')
     
-    return test_loss_first, test_loss_second, acc, correct
+    return test_loss_first, test_loss_second, accuracy, correct
+
+
+
+
+# Testing
+if __name__=='__main__':
+    from utils.config_model import *
+    from utils.dataloader import *
+    dataset_name = "rsna-bone-age" # rsna-bone-age-kaggle or rsna-bone-age
+    basedOnSex = False
+    gender='male'
+
+    train_dataset , test_dataset = data_handler(dataset_name = dataset_name, defualt_path = '', 
+                                        basedOnSex = basedOnSex, gender = gender, transform_action = 'train', target_type = 'onehot')
+    num_classes = train_dataset.num_classes 
+
+    _, _, test_loader = data_wrapper(train_dataset = train_dataset, 
+                            test_dataset = test_dataset, 
+                            batch_size = 1,
+                            test_val_batch_size = 1, 
+                            shuffle = False, num_workers = 1)
+    
+    # Select and import Model
+    net = MobileNet_V3(pretrained = True, image_channels = 1, num_classes = train_dataset.num_classes).cuda()
+    reload_model(net, "./ResultModels/20220523_110557_MobileNetV3_Pre/checkpoint_epoch25.pth")
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    criterion = loss_funcion('bce_wl')
+    
+    print(evaluate(net, None, test_loader, device, criterion))
