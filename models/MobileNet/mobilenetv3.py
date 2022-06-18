377175e7beadd571b0d412cfead73c64b3143f3e
Mohammad Mahdi Samei <9259samei@gmail.com>
2022-06-18 16:46:55 +0430

    Update MobileNetV3, A seperate fc for gender available with gender type fc (Ready to use and choose with get args).

--- models/MobileNet/mobilenetv3.py
+++ models/MobileNet/mobilenetv3.py
@@ -14,6 +14,7 @@ class MobileNetV3(torch.nn.Module):
         pretrained: bool = True,
         input_size: int = 2,
         name_suffix: str = '',
+        gender_fc_type: bool = False,
     ) -> None:
         """MobileNetV3 class.
 
@@ -32,19 +33,26 @@ class MobileNetV3(torch.nn.Module):
         self.in_channels = image_channels
         self.num_classes = num_classes
 
-        self.mobilenet_v2 = models.mobilenet_v3_large(pretrained=pretrained)
+        self.mobilenet_v3 = models.mobilenet_v3_large(pretrained=pretrained)
 
-        self.mobilenet_v2.features[0][0] = torch.nn.Conv2d(image_channels, 16, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False)
+        self.mobilenet_v3.features[0][0] = torch.nn.Conv2d(image_channels, 16, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False)
 
         self.avgpool = torch.nn.AdaptiveAvgPool2d(1)
 
-        in_features = self.mobilenet_v2.classifier[0].in_features
+        in_features = self.mobilenet_v3.classifier[0].in_features
         self.add_feature = 0 if input_size <= 1 else input_size - 1
 
-        self.mobilenet_v2.classifier = torch.nn.Sequential(
+        self.mobilenet_v3.fc_gender = torch.nn.Sequential(
+
+            torch.nn.Linear(1, 16),
+            torch.nn.Hardswish(inplace=True),
+        )
+        self.fc_gender_feature = 16 if gender_fc_type else 1
+
+        self.mobilenet_v3.classifier = torch.nn.Sequential(
 
             # torch.nn.Dropout(0.2),
-            torch.nn.Linear(in_features + 1, 2048),
+            torch.nn.Linear(in_features + self.fc_gender_feature, 2048),
             torch.nn.Hardswish(inplace=True),
             torch.nn.Dropout(0.2),
             torch.nn.Linear(2048, 2048),
@@ -70,15 +78,18 @@ class MobileNetV3(torch.nn.Module):
             y = x[1]
             x = x[0]
         
-        x = self.mobilenet_v2.features(x)
+        x = self.mobilenet_v3.features(x)
 
         x = self.avgpool(x)
         x = torch.flatten(x, 1)
 
+        if self.fc_gender_feature:
+            y = self.mobilenet_v3.fc_gender(y)
+
         if self.add_feature > 0:
             x = torch.cat((x, y), dim=1)
 
-        x = self.mobilenet_v2.classifier(x)
+        x = self.mobilenet_v3.classifier(x)
 
         return x
 
