0208fab015cc368147f1bb79099117afd61bb42e
Mohammad Mahdi Samei <9259samei@gmail.com>
2022-06-23 19:29:50 +0430

    WandB run uploaded and cleaned.

---
+++ wandb/offline-run-20220620_105230-2e3d4n4e/tmp/code/optimize_loss.py
@@ -0,0 +1,37 @@
+from pickletools import optimize
+import torch
+
+
+
+
+def loss_funcion(type='mse'):
+    if type == 'mse':
+        return torch.nn.MSELoss()
+    if type == 'mae':
+        return torch.nn.L1Loss()
+    elif type == 'bce_wl':
+        return torch.nn.BCEWithLogitsLoss()
+    elif type == 'bce':
+        return torch.nn.BCELoss()
+
+def optimizer_loader(net, learning_rate: int = 0.001, amp: bool = False):
+
+    # Defining the optimizer
+    optimizer = torch.optim.Adam(
+                            net.parameters(), 
+                            lr=learning_rate, 
+                            betas=(0.5, 0.999), 
+                            weight_decay=1e-8,
+                            )
+
+    # Defining the scheduler
+    # goal: maximize Dice score
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(
+                                                    optimizer, 
+                                                    'max', 
+                                                    patience = 2)
+    grad_scaler = torch.cuda.amp.GradScaler(enabled = amp)
+
+    return optimizer, scheduler, grad_scaler
+
+
